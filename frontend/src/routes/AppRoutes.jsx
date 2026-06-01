147dd236fb3e80411216a4bea9082d6c262000d0
kdev_sobreira <kauam.morais09@gmail.com>
2026-05-31 23:48:09 -0300

    feat: Adicionando cores globais e rotas iniciais

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import Login from '../pages/Login/Login';
+import Cadastro from '../pages/Cadastro/Cadastro';
+
+
+
+function AppRoutes() {
+  return (
+    <Routes>
+      
+      <Route path="/" element={<Navigate to="/login" replace />} />
+
+      
+      <Route path="/login" element={<Login />} />
+      
+      
+      <Route path="*" element={<div style={{ color: '#fff', padding: '20px' }}>Página não encontrada (404)</div>} />
+    </Routes>
+  );
+}
+
+export default AppRoutes;
