6ffac4479968c6b43a6778aeeda037c9018454ea
SergioCostaczr <manoelsergio.dev@gmail.com>
2026-06-04 23:21:34 -0300

    chore: prepara validacao e docs do backend

--- backend/src/test/java/com/github/sangueamigo/SangueAmigoApplicationTests.java
+++ backend/src/test/java/com/github/sangueamigo/SangueAmigoApplicationTests.java
@@ -3,7 +3,14 @@ package com.github.sangueamigo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.datasource.url=jdbc:h2:mem:sangueamigo-test;MODE=PostgreSQL;DB_CLOSE_DELAY=-1",
+		"spring.datasource.driver-class-name=org.h2.Driver",
+		"spring.datasource.username=sa",
+		"spring.datasource.password=",
+		"spring.jpa.hibernate.ddl-auto=create-drop",
+		"spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect"
+})
 class SangueAmigoApplicationTests {
 
 	@Test
